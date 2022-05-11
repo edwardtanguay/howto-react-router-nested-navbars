6101a271bbef9dc715922067fe65223a2a83c52a
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 00:09:26 +0200

    three book groups in

---
+++ src/components/BookGroupFantasy.jsx
@@ -0,0 +1,8 @@
+export const BookGroupFantasy = () => {
+	return (
+		<>
+			<h3>Fantasy Books</h3>
+			<p>I've read fantasy books since a child.</p>
+		</>
+	);
+};
---
+++ src/components/BookGroupPolitics.jsx
@@ -0,0 +1,8 @@
+export const BookGroupPolitics = () => {
+	return (
+		<>
+		 <h3>Political Books</h3>
+			<p>These are books about society and politics.</p>
+		</>
+	);
+};

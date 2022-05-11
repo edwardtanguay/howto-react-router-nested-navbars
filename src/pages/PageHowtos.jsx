05dd7aa8cf3401283c7fd50a77d0870bb1e0b67d
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 00:54:39 +0200

    showings titles

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
 
 export const PageHowtos = () => {
 	const [howtos, setHowtos] = useState([]);
 
+useEffect(() => {
+	(async () => {
+		setHowtos((await axios.get(url)).data);
+	})();
+}, []);
+
 	return (
 		<>
 			<h2>Howtos</h2>
 			<p>These are my howtos:</p>
-			<ul className="howtos">
+			<div className="howtos">
 				{howtos.map((howto, index) => {
-					return (
-						<li key={index}>nnn</li>
-					)
-				})}	
-			</ul>
+					return <div className="howto" key={index}>{howto.title}</div>;
+				})}
+			</div>
 		</>
 	);
 };

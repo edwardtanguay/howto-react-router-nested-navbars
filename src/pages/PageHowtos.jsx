997b36620b9632c295ebe8df7c2d094c65c29460
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 01:12:27 +0200

    listing php items

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -6,19 +6,30 @@ const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
 export const PageHowtos = () => {
 	const [howtos, setHowtos] = useState([]);
 
-useEffect(() => {
-	(async () => {
-		setHowtos((await axios.get(url)).data);
-	})();
-}, []);
+	useEffect(() => {
+		(async () => {
+			const _howtos = (await axios.get(url)).data;
+			const _filteredHowtos = _howtos.filter(m => m.category.toLowerCase() === 'php');
+			setHowtos(_filteredHowtos);
+		})();
+	}, []);
 
 	return (
 		<>
 			<h2>Howtos</h2>
-			<p>These are my howtos:</p>
+			<p>These are my PHP howtos:</p>
 			<div className="howtos">
 				{howtos.map((howto, index) => {
-					return <div className="howto" key={index}>{howto.title}</div>;
+					return (
+						<div className="howto" key={index}>
+							<div className="title">
+								<a target="_blank" href={`https://edwardtanguay.netlify.app/howtos?id=${howto.id}`}>
+									{howto.title}
+								</a>
+							</div>
+							<pre className="body">{howto.body}</pre>
+						</div>
+					);
 				})}
 			</div>
 		</>

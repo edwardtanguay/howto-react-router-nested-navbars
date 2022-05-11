7011b3665a1ebe32ee34bdd94f5242f166a841f8
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 01:27:23 +0200

    showing php navlinks

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
 
@@ -9,21 +10,40 @@ export const PageHowtos = () => {
 	useEffect(() => {
 		(async () => {
 			const _howtos = (await axios.get(url)).data;
-			const _filteredHowtos = _howtos.filter(m => m.category.toLowerCase() === 'php');
+			const _filteredHowtos = _howtos.filter(
+				(m) => m.category.toLowerCase() === 'php'
+			);
 			setHowtos(_filteredHowtos);
 		})();
 	}, []);
 
 	return (
-		<>
+		<div className="page_howtos">
 			<h2>Howtos</h2>
 			<p>These are my PHP howtos:</p>
+
+			<nav>
+				{howtos.map((howto, index, arr) => {
+					return (
+						<>
+							<div className="navlink">{howto.title}</div>
+							{index < arr.length - 1 && (
+								<span className="separator">|</span>
+							)}
+						</>
+					);
+				})}
+			</nav>
+
 			<div className="howtos">
 				{howtos.map((howto, index) => {
 					return (
 						<div className="howto" key={index}>
 							<div className="title">
-								<a target="_blank" href={`https://edwardtanguay.netlify.app/howtos?id=${howto.id}`}>
+								<a
+									target="_blank"
+									href={`https://edwardtanguay.netlify.app/howtos?id=${howto.id}`}
+								>
 									{howto.title}
 								</a>
 							</div>
@@ -32,6 +52,6 @@ export const PageHowtos = () => {
 					);
 				})}
 			</div>
-		</>
+		</div>
 	);
 };

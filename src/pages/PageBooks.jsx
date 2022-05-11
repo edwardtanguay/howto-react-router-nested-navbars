7c6907704548ab0fec6e71d54eecae3cbca7c52b
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 00:27:56 +0200

    subnav works

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,15 +1,35 @@
 import { BookGroupFantasy } from '../components/BookGroupFantasy';
 import { BookGroupPolitics } from '../components/BookGroupPolitics';
 import { BookGroupTechnology } from '../components/BookGroupTechnology';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 export const PageBooks = () => {
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
-			<p>This is the book page.</p>
-			<BookGroupFantasy />
-			<BookGroupPolitics />
-			<BookGroupTechnology />
+			<p>The books I have are in three categories:</p>
+
+			<ul>
+				<li>
+					<NavLink to="fantasy">Fantasy</NavLink>
+				</li>
+				<li>
+					<NavLink to="politics">Politics</NavLink>
+				</li>
+				<li>
+					<NavLink to="technology">Technology</NavLink>
+				</li>
+			</ul>
+			<hr />
+			<Routes>
+				<Route path="fantasy" element={<BookGroupFantasy />} />
+				<Route path="politics" element={<BookGroupPolitics />} />
+				<Route path="technology" element={<BookGroupTechnology />} />
+				<Route
+					path="/"
+					element={<Navigate to="/books/fantasy" replace />}
+				/>
+			</Routes>
 		</div>
 	);
 };
